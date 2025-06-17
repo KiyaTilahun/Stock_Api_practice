9ae65215f919b5184fe21dcbf64feaf6e31dbd40
kiyatilahun <kiyatilahun0@gmail.com>
2025-06-17 07:50:01 +0300

    Add .vs/ and other generated files to .gitignore and untrack existing ones

---
+++ Dtos/Accounts/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspApi.Dtos.Accounts
+{
+    public class LoginDto
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Dtos/Accounts/NewUserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AspApi.Dtos.Accounts
+{
+    public class NewUserDto
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+    }
+}
